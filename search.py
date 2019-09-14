88856fab63b91627559a11c150fc515c93265a00
Vikram N. Subramanian <vikram.subramanian6@gmail.com>
2019-09-14 13:24:07 -0400

    fully working, only for python tho

--- search.py
+++ search.py
@@ -1,10 +1,34 @@
 import subprocess
+import os
+import json
+import pprint
+import requests
 
 
-def fetchData(search_words):
-
+def fetchData(search_words, results_no, language):
+    """return file of dicts with the format {urlto raw:}"""
+    returnData = []
+    lang_no = {"python": "19"}
     cmd = "https://searchcode.com/api/codesearch_I/?q="
-    for search_word in search_words
-    cmd += search_word + "+"
+    for search_word in search_words:
+        cmd += search_word + "+"
     cmd = cmd[:-1]
-    cmd += "&p=1&per_page=100"
+    cmd += "&p=1&per_page=100&lan="+lang_no[language]
+    print(cmd)
+
+    r = requests.get(cmd)
+    print(r.status_code)
+    data = r.json()
+
+    for result in range(len(data["results"])):
+        if result < results_no:
+            vals = {}
+            link = data["results"][result]["url"]
+            link = link.replace("view", "raw")
+            vals["url"] = link
+            vals["code"] = data["results"][result]["lines"]
+        returnData.append(vals)
+
+    # print(data["matchterm"])
+
+    return vals
