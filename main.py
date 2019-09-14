d58691585f81b28d01cbed01ef6a2fd0586ed9e5
Curtis Chong <curtischong5@gmail.com>
2019-09-14 13:56:17 -0400

    successfully parses data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+import json
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello, World!'
+  print(request.data.decode("utf-8") )
+  data = json.loads(request.data.decode("utf-8"))
+  return 'Hello, World!' + data['language']
